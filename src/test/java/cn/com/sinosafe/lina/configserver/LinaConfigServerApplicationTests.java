f7f3b2f36a0332216338516f13afed62a601d704
yangpan1 <yangpan1@10.11.32.227>
2017-09-22 17:00:23 +0800

    commit lina config server

---
+++ src/test/java/cn/com/sinosafe/lina/configserver/LinaConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.com.sinosafe.lina.configserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LinaConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
